a248f9e7cbbf8f60aac64838db01bbd1df61bea0
ganesh-pandey <ganesh.pandey_cs19@gla.ac.in>
2020-07-18 15:05:51 -0400

    user input

--- src/first/Scan.java
+++ src/first/Scan.java
@@ -6,5 +6,11 @@ public class Scan {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("this is my first file");
+        System.out.println("enter your name");
+        String name = scan.nextLine();
+        System.out.println("enter the your code");
+        short code = scan.nextShort();
+        System.out.println("name: " + name);
+        System.out.println("code: " + code);
     }
 }
