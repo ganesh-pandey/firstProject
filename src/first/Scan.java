ea63e9448ac4ceb4bc2243afd47a9fefb734e2b2
ganesh-pandey <ganesh.pandey_cs19@gla.ac.in>
2020-07-18 14:50:48 -0400

    commit

---
+++ src/first/Scan.java
@@ -0,0 +1,10 @@
+package first;
+
+import java.util.Scanner;
+
+public class Scan {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("this is my first file");
+    }
+}
